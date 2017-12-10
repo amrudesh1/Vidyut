c2e35360108d52621204c25eea57748e50089471
amrudesh1 <amrbal@gmail.com>
2017-12-11 01:19:05 +0530

    App Splash Screen

--- app/src/main/java/com/example/amrudesh/vidyut/Main2Activity.java
+++ app/src/main/java/com/example/amrudesh/vidyut/Main2Activity.java
@@ -1,13 +1,39 @@
 package com.example.amrudesh.vidyut;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+public class Main2Activity extends AppCompatActivity {
+    LinearLayout l1,l2;
+    Button btnsub;
+    Animation uptodown,downtoup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btnsub = (Button)findViewById(R.id.buttonsub);
+        l2 = (LinearLayout) findViewById(R.id.l1);
+        l1 = (LinearLayout) findViewById(R.id.L2);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
     }
 }
